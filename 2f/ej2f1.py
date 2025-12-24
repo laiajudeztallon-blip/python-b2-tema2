4d36fcce0c8d7aeb4e80f5d8a770d3b9dcf30d26
laiajudeztallon-blip <laiajudeztallon@gmail.com>
2025-12-24 10:49:44 +0100

    Update ej2f1.py

--- 2f/ej2f1.py
+++ 2f/ej2f1.py
@@ -51,17 +51,27 @@ from pandas.core.frame import DataFrame
 
 def create_histograms(df: DataFrame, features: List[str]) -> Figure:
     # Write here your code
-    pass
+    fig, axs = plt.subplots(2, 3, figsize=(15, 10))
+    for i, feature in enumerate(features):
+        ax = axs[i // 3, i % 3]
+        sns.histplot(df, x=feature, hue="target", ax=ax, kde=True)
+    plt.tight_layout()
+    return fig
 
 
 def save_img_pickle(fig: Figure, filename: str) -> None:
     # Write here your code
-    pass
+    with open(filename, "wb") as f:
+        pickle.dump(fig, f)
+    plt.close(fig)
+    return True
 
 
 def load_and_display_figure(filename: str) -> Figure:
     # Write here your code
-    pass
+    with open(filename, "rb") as f:
+        fig = pickle.load(f)
+    return fig
 
 
 # Para probar el código, descomenta las siguientes líneas
