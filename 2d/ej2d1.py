3cf6780782855578013839df35142036c95fb3a2
laiajudeztallon-blip <laiajudeztallon@gmail.com>
2025-12-24 10:31:17 +0100

    Update ej2d1.py

--- 2d/ej2d1.py
+++ 2d/ej2d1.py
@@ -37,22 +37,26 @@ from scipy import stats
 
 def calculate_mean(file_path: str) -> float:
     # Write here your code
-    pass
+    data = pd.read_csv(file_path)
+    return data["Calificaciones"].mean()
 
 
 def calculate_mean(file_path: str) -> float:
     # Write here your code
-    pass
+    data = pd.read_csv(file_path)
+    return data["Calificaciones"].var()
 
 
 def calculate_skewness(file_path: str) -> float:
     # Write here your code
-    pass
+    data = pd.read_csv(file_path)
+    return stats.skew(data["Calificaciones"])
 
 
 def calculate_kurtosis(file_path: str) -> float:
     # Write here your code
-    pass
+    data = pd.read_csv(file_path)
+    return stats.kurtosis(data["Calificaciones"])
 
 
 # Para probar el código, descomenta las siguientes líneas
